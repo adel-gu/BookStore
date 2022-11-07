011e9e39a931dfa97db16868abfd21f6ee8c88de
adelguit <adeelguitoun@gmail.com>
2022-11-07 16:47:53 +0100

    Implement books actions and reducer

---
+++ src/redux/books/books.js
@@ -0,0 +1,41 @@
+// Actions
+const ADD_BOOK = 'ADD_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+
+// Initial state
+const books = [];
+
+// Reducer
+const BooksReducer = (state = books, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return state.push(action.book);
+    case REMOVE_BOOK:
+      return state.map((book) => {
+        if (book.id !== action.id) {
+          return book;
+        }
+      });
+    default:
+      return state;
+  }
+};
+
+// Action Creators
+const addBookAct = (book) => {
+  return {
+    type: ADD_BOOK,
+    book: book,
+  };
+};
+
+const removeBookAct = (bookID) => {
+  return {
+    type: REMOVE_BOOK,
+    id: bookID,
+  };
+};
+
+// Exports
+export default BooksReducer;
+export { addBookAct, removeBookAct };
