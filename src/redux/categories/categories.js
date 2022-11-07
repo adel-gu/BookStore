560234b289f1c5dfe3fb9031787c45fc20b6e1ec
adelguit <adeelguitoun@gmail.com>
2022-11-07 17:54:39 +0100

    Implement categories redux

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,25 @@
+// Actions
+const STATUS_CHECK = 'bookstore/categories/STATUS_CHECK';
+
+// Initial state
+const categories = [];
+
+// Reducer
+const categoriesReducer = (state = categories, action) => {
+  switch (action.type) {
+    case STATUS_CHECK:
+      return 'Under construction';
+    default:
+      return state;
+  }
+};
+
+// Action Creatores
+export const statusCheckAct = () => {
+  return {
+    type: STATUS_CHECK,
+  };
+};
+
+// Exports
+export default categoriesReducer;
