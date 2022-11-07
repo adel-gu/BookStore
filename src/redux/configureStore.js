a6edf0489fa1988d3e75b91388a604598d0d267b
adelguit <adeelguitoun@gmail.com>
2022-11-07 19:25:33 +0100

    Combine both reducers using configureStore function

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,15 @@
-import { combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
 // Reducers
 import BooksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
-// Combining reducers
-const rootReducer = combineReducers({
-  books: BooksReducer,
-  categories: categoriesReducer,
+// Configure Store
+const store = configureStore({
+  reducer: {
+    books: BooksReducer,
+    categories: categoriesReducer,
+  },
 });
 
-export default rootReducer;
+export default store;
