8e69c2efc00b63183b5ad62a511bc1b3410710f0
adelguit <adeelguitoun@gmail.com>
2022-11-08 11:20:51 +0100

    Implement the adding book functionality

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,17 +1,40 @@
+// UseState
+import { useState } from "react";
+//  Dispatch
+import { useDispatch } from "react-redux";
+// addBook Component
+import { addBook } from "../redux/books/books";
+// Id provider library
+import {v4 as uuidv4} from 'uuid';
+
 const AddBook = (props) => {
+  const [book, setBook] = useState({id:uuidv4()})
+  
+  const dispatch = useDispatch();
+
+  const getInputValue = (e) => {
+    setBook({
+      ...book,
+      [e.target.name]: e.target.value,
+    })
+  }
+  
   return (
-    <form action="/action_page.php">
+    <form action="/action_page.php" onSubmit={(e) => {
+      e.preventDefault()
+      dispatch(addBook(book))
+      }}>
       <fieldset>
         <legend>Add New Book</legend>
         <ul>
           <li>
-            <input type="text" name="title" placeholder="Book title"/>
+            <input type="text" name="title" placeholder="Book title" onChange={(e) => getInputValue(e)}/>
           </li>
           <li>
-            <input type="text" name="author" placeholder="Author"/>
+            <input type="text" name="author" placeholder="Author" onChange={(e) => getInputValue(e)}/>
           </li>
           <li>
-            <button type="button">Add Book</button>
+            <button type="submit" >Add Book</button>
           </li>
         </ul>
       </fieldset>
