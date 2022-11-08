74c91e770ad4e14605c138ffe052009dd77164a6
adelguit <adeelguitoun@gmail.com>
2022-11-08 10:33:40 +0100

    Add redux provider and use the store data

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
 // React
 import { Routes, Route } from 'react-router-dom';
+// redux provider
+import { Provider } from 'react-redux';
+// Store
+import store from './redux/configureStore';
 
 import Navbar from './components/NavBar';
 import Categories from './pages/Categories';
 import Books from './pages/Books';
 
 const App = () => (
-  <>
+  <Provider store={store}>
     <Navbar />
     <Routes>
-      <Route path="/" element={<Books />} />
-      <Route path="categories" element={<Categories />} />
+      <Route path='/' element={<Books />} />
+      <Route path='categories' element={<Categories />} />
     </Routes>
-  </>
+  </Provider>
 );
 
 export default App;
