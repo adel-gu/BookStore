36aed21faa576b3e6af1c2a5a5b57fbac739b351
adelguit <adeelguitoun@gmail.com>
2022-11-08 10:22:56 +0100

    Update books array with few data about books

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,7 +3,18 @@ const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
 // Initial state
-const books = [];
+const books = [
+  {
+    id: 0,
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+  },
+  {
+    id: 1,
+    title: 'Dune',
+    author: 'Frank Herbet',
+  },
+];
 
 // Reducer
 const BooksReducer = (state = books, action) => {
