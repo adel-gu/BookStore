17253ef807d42d9f87aeba0396d00a663acaa9dc
adelguit <adeelguitoun@gmail.com>
2022-11-08 11:23:26 +0100

    Empty the books initial state array

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,18 +3,7 @@ const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
 // Initial state
-const books = [
-  {
-    id: 0,
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-  },
-  {
-    id: 1,
-    title: 'Dune',
-    author: 'Frank Herbet',
-  },
-];
+const books = [];
 
 // Reducer
 const BooksReducer = (state = books, action) => {
