9808577f0e73de8b8f8e16d9481029c202fce276
adelguit <adeelguitoun@gmail.com>
2022-11-08 15:44:06 +0100

    Display status check when button clicked

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,3 +1,26 @@
-const Categories = () => <button type="button">Check status</button>;
+// Redux methods
+import { useSelector, useDispatch } from 'react-redux';
+// Status action creator
+import { statusCheck } from '../redux/categories/categories';
+
+const Categories = () => {
+  const status = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <button
+        type='button'
+        onClick={() => {
+          dispatch(statusCheck());
+          console.log(status);
+        }}
+      >
+        Check status
+      </button>
+      <h1>{status}</h1>
+    </>
+  );
+};
 
 export default Categories;
