dcb8132f5111568fd7616e533066dbbe7f6f94d4
adelguit <adeelguitoun@gmail.com>
2022-11-11 21:48:09 +0100

    Update App file

--- src/app.js
+++ src/app.js
@@ -2,19 +2,20 @@
 import { Routes, Route } from 'react-router-dom';
 // redux provider
 import { Provider } from 'react-redux';
+
 // Store
 import store from './redux/configureStore';
-
+// Components
 import Navbar from './components/NavBar';
 import Categories from './pages/Categories';
-import Books from './pages/Books';
+import Books from './pages/Home';
 
 const App = () => (
   <Provider store={store}>
     <Navbar />
     <Routes>
-      <Route path="/" element={<Books />} />
-      <Route path="categories" element={<Categories />} />
+      <Route path='/' element={<Books />} />
+      <Route path='categories' element={<Categories />} />
     </Routes>
   </Provider>
 );
