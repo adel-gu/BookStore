f5d774c9cfabb059c5b1c80f402498e331e3f120
adelguit <adeelguitoun@gmail.com>
2022-11-11 21:45:40 +0100

    Create booksSlice and loading books thunk

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,70 +1,29 @@
-import axios from 'axios';
-
-// API
-const APP_ID = 'VTajB0F8HFH8ssRHytO8';
-export const API_URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${APP_ID}/books`;
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+// Working with API
+import BooksService from '../../services/BooksService';
 
 // Actions
-const ADD_BOOK = 'bookstore/books/ADD_BOOK';
-const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
-const LOAD_BOOKS = 'bookstore/books/LOAD_BOOKS';
+const ADD_BOOK = 'books/ADD_BOOK';
+const REMOVE_BOOK = 'books/REMOVE_BOOK';
+const LOAD_BOOKS = 'books/LOAD_BOOKS';
 
-// Action Creators
-export const loadBooks = (payload) => ({
-  type: LOAD_BOOKS,
-  payload,
+// Thunks
+export const loadBooks = createAsyncThunk(LOAD_BOOKS, async () => {
+  const res = await BooksService.getBooks();
+  return res.data;
 });
 
-export const addBook = (payload) => ({
-  type: ADD_BOOK,
-  payload,
-});
-
-export const removeBook = (payload) => ({
-  type: REMOVE_BOOK,
-  payload,
-});
-
-// Thunk action creators
-export const loadBooksThunk = () => async (dispatch) => {
-  // fetch books from API
-  const response = await fetch(API_URL);
-  const books = await response.json();
-  dispatch(loadBooks(books));
-};
-
-const postBookThunk = async () => {
-  // fetch books from API
-  await fetch(API_URL, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      item_id: 4,
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    }),
-  });
-};
-// postBookThunk();
-// loadBooksThunk()();
-
-// Initial state
-const books = {};
-
-// Reducer
-const BooksReducer = (state = books, action) => {
-  switch (action.type) {
-    case LOAD_BOOKS:
+// Slice
+const booksSlice = createSlice({
+  name: 'bookstore',
+  initialState: {},
+  extraReducers: (builder) => {
+    // Loading Books
+    builder.addCase(loadBooks.fulfilled, (state, action) => {
       return action.payload;
-    case ADD_BOOK:
-      return [...state, action.payload];
-    case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload);
-    default:
-      return state;
-  }
-};
+    });
+  },
+});
 
 // Exports
-export default BooksReducer;
+export default booksSlice;
