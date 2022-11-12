71ca23176e4dc0331be341ed913a4d5083907954
adelguit <adeelguitoun@gmail.com>
2022-11-12 19:05:27 +0100

    Style NavBar element

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,36 @@
-import {Link} from 'react-router-dom'
+import {LinkContainer} from 'react-router-bootstrap';
+import {Container, Nav, Navbar} from 'react-bootstrap';
+import {PersonFill} from "react-bootstrap-icons";
 
-const Navbar = () => {
+// Style Sheet
+import './NavBar.css';
+
+const NavbarBookStore = () => {
   return (
     <header>
-      <a><h3>BookeStore CMS</h3></a>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/categories">Categories</Link></li>
-        </ul>
-        <a href="">profile</a>
-      </nav>
+      <Navbar expand="lg" className="bg-white">
+          <Container>
+            <Navbar.Brand className="logo fw-bold">Bookstore CMS</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto align-items-center w-100">
+                <LinkContainer to="/">
+                  <Nav.Link className="book-nav-link">BOOKS</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/categories">
+                  <Nav.Link className="book-nav-link">CATEGORIES</Nav.Link>              
+                </LinkContainer>
+                <Nav.Link className="ms-auto">
+                  <div className='border rounded-circle d-flex p-1'>
+                    <PersonFill className='text-primary'/>
+                  </div>
+                </Nav.Link>                            
+              </Nav>
+            </Navbar.Collapse>          
+          </Container>
+      </Navbar>
     </header>
   )
 }
 
-export default Navbar;
+export default NavbarBookStore;
