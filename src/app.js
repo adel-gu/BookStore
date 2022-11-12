7eed16d1a05f4492152fc9ef444f6126bd930f1c
adelguit <adeelguitoun@gmail.com>
2022-11-12 19:15:53 +0100

    Style the add book form section

--- src/app.js
+++ src/app.js
@@ -6,19 +6,16 @@ import { Provider } from 'react-redux';
 // Store
 import store from './redux/configureStore';
 // Components
-import Navbar from './components/NavBar';
+import NavbarBookStore from './components/NavBar';
 import Categories from './pages/Categories';
 import Books from './pages/Home';
 
-// StyleSheet
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 const App = () => (
   <Provider store={store}>
-    <Navbar />
+    <NavbarBookStore />
     <Routes>
       <Route path='/' element={<Books />} />
-      <Route path='categories' element={<Categories />} />
+      <Route path='/categories' element={<Categories />} />
     </Routes>
   </Provider>
 );
