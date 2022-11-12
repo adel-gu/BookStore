916be0b208f3e6fd7962cad90659c505296e87e6
adelguit <adeelguitoun@gmail.com>
2022-11-12 02:36:54 +0100

    Update thunks

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { API_URL } from '../../http-common';
+
 // Working with API
 import BooksService from '../../services/BooksService';
 
@@ -8,58 +8,46 @@ const ADD_BOOK = 'books/ADD_BOOK';
 const REMOVE_BOOK = 'books/REMOVE_BOOK';
 const LOAD_BOOKS = 'books/LOAD_BOOKS';
 
-// Thunks
-// Load Books
+// Load Books Thunk
 export const loadBooks = createAsyncThunk(LOAD_BOOKS, async () => {
   try {
-    const res = await BooksService.getBooks();
+    const res = await BooksService.getBooksServ();
     return res.data;
   } catch (err) {
-    throw new Error('somthing went wrong!!');
+    throw new Error(err);
   }
 });
 
-// Add a Book
+// Add a Book Thunk
 export const addBook = createAsyncThunk(ADD_BOOK, async (payload, thunkAPI) => {
-  await fetch(API_URL, {
-    method: 'POST',
-    body: JSON.stringify(payload),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  thunkAPI.dispatch(loadBooks());
+  try {
+    await BooksService.postBookServ(payload);
+    thunkAPI.dispatch(loadBooks());
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
-// Remove a book
+// Remove a book Thunk
 export const removeBook = createAsyncThunk(
   REMOVE_BOOK,
   async (payload, thunkAPI) => {
-    await fetch(`${API_URL}/${payload}`, {
-      method: 'DELETE',
-    });
-    thunkAPI.dispatch(loadBooks());
+    try {
+      await BooksService.delBookServ(payload);
+      thunkAPI.dispatch(loadBooks());
+    } catch (err) {
+      throw new Error(err);
+    }
   }
 );
 
-// Slice
+// Book Store Slice
 const booksSlice = createSlice({
   name: 'bookstore',
   initialState: {},
-  reducers: {
-    fetchBooks: (state) => {
-      return state;
-    },
-  },
   extraReducers: (builder) => {
     // Loading Books
-    builder.addCase(loadBooks.fulfilled, (state, action) => {
-      return action.payload;
-    });
-    // Adding a book
-    builder.addCase(addBook.fulfilled, (state, action) => {});
-    // Remove a book
-    builder.addCase(removeBook.fulfilled, (state, action) => {});
+    builder.addCase(loadBooks.fulfilled, (state, action) => action.payload);
   },
 });
 
